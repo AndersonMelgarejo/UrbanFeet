be55bf1dbe5bd949db7a219b4cf016f6320c9388
renzito0637d <renzito0637d@gmail.com>
2024-12-10 10:08:33 -0500

    Añadido direcionamiento al footer

--- funcionFooter.js
+++ funcionFooter.js
@@ -5,10 +5,10 @@ function loadFooter() {
           <div class="footerMenu">Redes</div>
           <div>
             <ul class="MenuOpciones">
-              <li>Facebook</li>
-              <li>Instagram</li>
-              <li>Twitter</li>
-              <li>Tiktok</li>
+              <li><a href="https://www.facebook.com/nike" class="white">Facebook</a></li>
+              <li><a href="RedesSociales.html" class="white">Instagram</a></li>
+              <li><a href="RedesSociales.html" class="white">Twitter</a></li>
+              <li><a href="RedesSociales.html" class="white">Tiktok</a></li>
             </ul>
           </div>
         </div>
@@ -17,13 +17,13 @@ function loadFooter() {
           <div class="footerMenu">Menú</div>
           <div>
             <ul class="MenuOpciones">
-              <li>Inicio</li>
-              <li>Nosotros</li>
-              <li>Redes Sociales</li>
-              <li>Ubícanos</li>
-              <li>Contáctanos</li>
-              <li>Libro de Reclamaciones</li>
-              <li>Carrito</li>
+              <li><a href="inicio.html" class="white">Inicio</a></li>
+              <li><a href="Nosotros.html" class="white">Nosotros</a></li>
+              <li><a href="RedesSociales.html" class="white">Redes Sociales</a></li>
+              <li><a href="Contactanos.html" class="white">Ubícanos</a></li>
+              <li><a href="Contactanos.html" class="white">Contáctanos</a></li>
+              <li><a href="Contactanos.html" class="white">Libro de Reclamaciones</a></li>
+              <li><a href="carrito.html" class="white">Carrito</a></li>
             </ul>
           </div>
         </div>
@@ -32,10 +32,10 @@ function loadFooter() {
           <div class="footerMenu">Contáctanos</div>
           <div>
             <ul class="MenuOpciones">
-              <li>946531247</li>
-              <li>contacto@urbanfeet.com</li>
-              <li>Horario de atención:</li>
-              <li>Lunes a Viernes, 9:00 AM - 6:00 PM</li>
+              <li class="white">946531247</li>
+              <li class="white">contacto@urbanfeet.com</li>
+              <li class="white">Horario de atención:</li>
+              <li class="white">Lunes a Viernes, 9:00 AM - 6:00 PM</li>
             </ul>
           </div>
         </div>
